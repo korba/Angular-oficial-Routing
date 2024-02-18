f25bd6c024761e6150247b340e7272a757a5f86d
korba <pjpiotrowski@gmail.com>
2024-02-18 13:11:27 +0100

    compodoc

---
+++ .angular/cache/17.1.3/vite/deps/@angular_animations.js
@@ -0,0 +1,52 @@
+import {
+  AUTO_STYLE,
+  AnimationBuilder,
+  AnimationFactory,
+  AnimationGroupPlayer,
+  AnimationMetadataType,
+  BrowserAnimationBuilder,
+  NoopAnimationPlayer,
+  animate,
+  animateChild,
+  animation,
+  group,
+  keyframes,
+  query,
+  sequence,
+  stagger,
+  state,
+  style,
+  transition,
+  trigger,
+  useAnimation,
+  ɵPRE_STYLE
+} from "./chunk-4KLHRMGJ.js";
+import "./chunk-5X7LUNTU.js";
+import "./chunk-M43NMAIA.js";
+import "./chunk-V2DXGMIT.js";
+import "./chunk-UKEHM6V6.js";
+import "./chunk-ZDOIMVJD.js";
+export {
+  AUTO_STYLE,
+  AnimationBuilder,
+  AnimationFactory,
+  AnimationMetadataType,
+  NoopAnimationPlayer,
+  animate,
+  animateChild,
+  animation,
+  group,
+  keyframes,
+  query,
+  sequence,
+  stagger,
+  state,
+  style,
+  transition,
+  trigger,
+  useAnimation,
+  AnimationGroupPlayer as ɵAnimationGroupPlayer,
+  BrowserAnimationBuilder as ɵBrowserAnimationBuilder,
+  ɵPRE_STYLE
+};
+//# sourceMappingURL=@angular_animations.js.map
